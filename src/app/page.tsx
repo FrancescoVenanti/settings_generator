55d06feee6bb608b90880f8e0a43600c8e1babf3
FrancescoVenanti <francesco.venant@gmail.com>
2024-10-14 16:42:58 +0200

    page fixes TODO preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import JsonForm from "@/components/jsonform";
+import Preview from "@/components/preview";
 import ThemeForm from "@/components/themeform";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
@@ -7,8 +8,18 @@ export default function Home() {
     <main className="flex items-stretch justify-start sm:items-start p-10 gap-10">
       <Tabs defaultValue="settings" className="w-1/2">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="settings">Settings</TabsTrigger>
-          <TabsTrigger value="theme">Theme</TabsTrigger>
+          <TabsTrigger
+            value="settings"
+            className=" data-[state=active]:text-white data-[state=active]:font-bold data-[state=active]:bg-violet-800"
+          >
+            Settings
+          </TabsTrigger>
+          <TabsTrigger
+            value="theme"
+            className=" data-[state=active]:text-white data-[state=active]:font-bold data-[state=active]:bg-violet-800"
+          >
+            Theme
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="settings">
           <JsonForm />
@@ -17,7 +28,9 @@ export default function Home() {
           <ThemeForm />
         </TabsContent>
       </Tabs>
-      <div className="w-1/2 bg-white/30 h-screen sticky top-0 rounded-md"></div>
+      <div className="w-1/2 bg-white/30 h-screen sticky top-0 rounded-md flex justify-center items-center">
+        <Preview />
+      </div>
     </main>
   );
 }
